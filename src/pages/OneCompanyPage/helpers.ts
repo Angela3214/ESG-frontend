9d56a49aa2a148edf8eae85e39005ff062ad598f
angel <angela_volkova_up@mail.ru>
2024-05-16 18:59:01 +0300

    Updated OneCompanyPage

--- src/pages/OneCompanyPage/helpers.ts
+++ src/pages/OneCompanyPage/helpers.ts
@@ -3,8 +3,13 @@ import { groupBy } from '../../lib/helpers';
 
 type TableData = {
   companyName: string;
-  year: string;
-  value: number;
+  year: number;
+  allLetters: string;
+  allTopics: string;
+  average: string;
+  environmental: string;
+  social: string;
+  governance: string;
 }[];
 
 export const formatTableData = (aggregatedData: IGetTopsisResponseItem[]): TableData => {
@@ -12,12 +17,39 @@ export const formatTableData = (aggregatedData: IGetTopsisResponseItem[]): Table
   const tableData: TableData = [];
 
   dataGroupedByModelName.forEach((items, companyName) => {
-    items.forEach((item) => {
-      tableData.push({
+    const dataByYear = groupBy(items, 'year');
+    dataByYear.forEach((yearData, year) => {
+      const yearEntry = {
         companyName,
-        year: item.year.toString(),
-        value: item.value ?? 0,  // Using 0 as default if no value is present
+        year: parseInt(year, 10),
+        allLetters: 'None',
+        allTopics: 'None',
+        average: 'None',
+        environmental: 'None',
+        social: 'None',
+        governance: 'None'
+      };
+
+      yearData.forEach(item => {
+        const valueFormatted = item.value ? item.value.toFixed(3) : 'None';
+        switch (item.aggregate_type) {
+          case 'all_letters':
+            yearEntry.allLetters = valueFormatted;
+            break;
+          case 'all_topics':
+            yearEntry.allTopics = valueFormatted;
+            break;
+          case 'average':
+            yearEntry.average = valueFormatted;
+            break;
+        }
+
+        if (item.environmental) yearEntry.environmental = item.environmental.toFixed(3);
+        if (item.social) yearEntry.social = item.social.toFixed(3);
+        if (item.governance) yearEntry.governance = item.governance.toFixed(3);
       });
+
+      tableData.push(yearEntry);
     });
   });
 
