c5fbcb43bb6e0ae3dc82167af07bfb123ef4bd0f
angel <angela_volkova_up@mail.ru>
2024-05-18 13:10:20 +0300

    Added colors for second page

--- src/pages/OneCompanyPage/helpers.ts
+++ src/pages/OneCompanyPage/helpers.ts
@@ -1,11 +1,8 @@
 import { IGetTopsisWithESGResponseItem } from '../../api/generated/data-contracts';
-import { groupBy } from '../../lib/helpers';
 
 type TableData = {
   companyName: string;
   year: number;
-  allLetters: string;
-  allTopics: string;
   average: string;
   environmental: string;
   social: string;
@@ -13,45 +10,12 @@ type TableData = {
 }[];
 
 export const formatTableData = (aggregatedData: IGetTopsisWithESGResponseItem[]): TableData => {
-  const dataGroupedByModelName = groupBy(aggregatedData, 'company_name');
-  const tableData: TableData = [];
-
-  dataGroupedByModelName.forEach((items, companyName) => {
-    const dataByYear = groupBy(items, 'year');
-    dataByYear.forEach((yearData, year) => {
-      const yearEntry = {
-        companyName,
-        year: parseInt(year, 10),
-        allLetters: 'None',
-        allTopics: 'None',
-        average: 'None',
-        environmental: 'None',
-        social: 'None',
-        governance: 'None'
-      };
-
-      yearData.forEach(item => {
-        const valueFormatted = item.value ? item.value.toFixed(3) : 'None';
-        switch (item.aggregate_type) {
-          case 'all_letters':
-            yearEntry.allLetters = valueFormatted;
-            break;
-          case 'all_topics':
-            yearEntry.allTopics = valueFormatted;
-            break;
-          case 'average':
-            yearEntry.average = valueFormatted;
-            break;
-        }
-
-        if (item.environmental) yearEntry.environmental = item.environmental.toFixed(3);
-        if (item.social) yearEntry.social = item.social.toFixed(3);
-        if (item.governance) yearEntry.governance = item.governance.toFixed(3);
-      });
-
-      tableData.push(yearEntry);
-    });
-  });
-
-  return tableData;
+  return aggregatedData.map(item => ({
+    companyName: item.company_name,
+    year: item.year,
+    average: item.value.toFixed(3),
+    environmental: item.environmental.toFixed(3),
+    social: item.social.toFixed(3),
+    governance: item.governance.toFixed(3),
+  }));
 };
