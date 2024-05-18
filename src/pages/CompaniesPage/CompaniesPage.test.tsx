d0d07d5a5cc77bf5df925fa8bda55bc69bf06569
Angela3214 <angela.volkova.up321@gmail.com>
2024-05-18 21:42:02 +0100

    Add integration tests for CompaniesPage

--- src/pages/CompaniesPage/CompaniesPage.test.tsx
+++ src/pages/CompaniesPage/CompaniesPage.test.tsx
@@ -1,27 +1,245 @@
+/* eslint-disable max-lines */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { CompaniesPage } from './CompaniesPage';
+import nock from 'nock';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import { CompaniesPage, allCompanyTypes } from './CompaniesPage';
 import { QueryClientProvider } from 'react-query';
 import { QueryClient } from 'react-query';
+import '@testing-library/jest-dom';
+import { configuration } from '../../api/controllers';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      staleTime: 10 * 60 * 1000,
-    },
-  },
-});
+function expectTableSkeletonToBePresent() {
+  expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
+  expect(screen.getByText('Список компаний')).toBeInTheDocument();
+
+  expect(screen.getAllByText('Поиск по названию компании')[0]).toBeInTheDocument();
+
+  expect(screen.getByText('Название компании')).toBeInTheDocument();
+  expect(screen.getByText('Отрасль')).toBeInTheDocument();
+
+  for (const companyType of allCompanyTypes) {
+    expect(screen.getAllByText(companyType)[0]).toBeInTheDocument();
+  }
+}
+
+function expectProgressBar() {
+  expect(screen.queryByTestId('loading')).toBeInTheDocument();
+  expect(screen.queryByText('Список компаний')).not.toBeInTheDocument();
+
+  expect(screen.queryByText('Поиск по названию компании')).not.toBeInTheDocument();
+
+  expect(screen.queryByText('Название компании')).not.toBeInTheDocument();
+  expect(screen.queryByText('Отрасль')).not.toBeInTheDocument();
+
+  for (const companyType of allCompanyTypes) {
+    expect(screen.queryByText(companyType)).not.toBeInTheDocument();
+  }
+}
 
 describe('Companies page', () => {
-  test('renders correctly', async () => {
+  const infiniteDelay = 2147483647;
+  let queryClient: QueryClient;
+  beforeEach(() => {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+          staleTime: Infinity,
+        },
+      },
+    });
+
+    jest.resetAllMocks();
+    nock.cleanAll();
+    nock.disableNetConnect();
+  });
+
+  afterEach(() => {
+    if (!nock.isDone()) {
+      console.log('Pending mocks: ', nock.pendingMocks());
+    }
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+
+  test('renders table on errors', async () => {
+    const apiMock = nock(configuration.baseHostName);
+    apiMock
+      .get('/api/bank/').reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/broker').reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/insurance').reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/mfo').reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+
     render(
       <QueryClientProvider client={queryClient}>
         <CompaniesPage />
-      </QueryClientProvider>
-      );
+      </QueryClientProvider>,
+    );
 
-    const elem = screen.getByText('Список компаний');
-    expect(elem).toBeInTheDocument();
+    await waitFor(() => {
+      expect(nock.isDone()).toBeTruthy();
+      expectTableSkeletonToBePresent();
+    });
+  });
+
+  test('renders table on empty response', async () => {
+    const apiMock = nock(configuration.baseHostName);
+    apiMock
+      .get('/api/bank/')
+      .reply(200, { 'items': [] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/broker')
+      .reply(200, { 'items': [] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/insurance')
+      .reply(200, { 'items': [] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/mfo')
+      .reply(200, { 'items': [] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CompaniesPage />
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(nock.isDone()).toBeTruthy();
+      expectTableSkeletonToBePresent();
+    });
+  });
+
+  test('renders all types of banks', async () => {
+    const apiMock = nock(configuration.baseHostName);
+    apiMock
+      .get('/api/bank/')
+      .reply(200, { 'items': [{ 'bank_name': 'THIS_IS_BANK' }] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/broker')
+      .reply(200, { 'items': [{ 'bank_name': 'THIS_IS_BROKER' }] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/insurance')
+      .reply(200, { 'items': [{ 'bank_name': 'THIS_IS_INSURANCE' }] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/mfo')
+      .reply(200, { 'items': [{ 'bank_name': 'THIS_IS_MFO' }] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CompaniesPage />
+      </QueryClientProvider>,
+    );
+
+    await waitFor(() => {
+      expect(nock.isDone()).toBeTruthy();
+      expectTableSkeletonToBePresent();
+
+      for (const bankName of ['THIS_IS_BANK', 'THIS_IS_BROKER', 'THIS_IS_INSURANCE', 'THIS_IS_MFO']) {
+        expect(screen.getByText(bankName)).toBeInTheDocument();
+      }
+    });
+  });
+
+  test('renders progress bar when on waiting', async () => {
+    const apiMock = nock(configuration.baseHostName);
+    apiMock
+      .get('/api/bank/')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/broker')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/insurance')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/mfo')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CompaniesPage />
+      </QueryClientProvider>,
+    );
+
+    await act(async () => {
+      return new Promise((r) => setTimeout(r, 500));
+    });
+    await waitFor(() => {
+      expectProgressBar();
+    });
+  });
+
+  test('renders table if at least one loads', async () => {
+    const apiMock = nock(configuration.baseHostName);
+    apiMock
+      .get('/api/bank/')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/broker')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/insurance')
+      .delayConnection(50)
+      .reply(200, { 'items': [] }, {
+        'Content-Type': 'application/json',
+      })
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+    apiMock
+      .get('/api/bank/mfo')
+      .delayConnection(infiniteDelay)
+      .reply(500)
+      .defaultReplyHeaders({ 'Access-Control-Allow-Origin': '*' });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <CompaniesPage />
+      </QueryClientProvider>,
+    );
+    await waitFor(() => {
+      expectTableSkeletonToBePresent();
+    });
   });
 });
--- src/pages/CompaniesPage/CompaniesPage.tsx
+++ src/pages/CompaniesPage/CompaniesPage.tsx
@@ -3,30 +3,33 @@ import { CircularProgress } from '@mui/material';
 import { useGetBanksQuery, useGetBrokersQuery, useGetInsurancesQuery, useGetMfoQuery } from '../../api/queries/bank';
 import CompaniesTable from '../../components/CompaniesTable';  // Import the new component
 
+export const allCompanyTypes = ['Страховая компания', 'Микрофинансовая организация', 'Брокерская компания', 'Банк'];
+
 export const CompaniesPage = () => {
   const { data: banksData, isLoading: loadingBanks } = useGetBanksQuery();
   const { data: brokersData, isLoading: loadingBrokers } = useGetBrokersQuery();
   const { data: mfoData, isLoading: loadingMfo } = useGetMfoQuery();
   const { data: insurancesData, isLoading: loadingInsurances } = useGetInsurancesQuery();
 
-  const isLoading = loadingBanks || loadingBrokers || loadingMfo || loadingInsurances;
+  const isLoading = loadingBanks && loadingBrokers && loadingMfo && loadingInsurances;
 
   const companies = useMemo(() => {
     const banks = banksData?.items.map(bank => ({ name: bank.bank_name, type: 'Банк' })) || [];
     const brokers = brokersData?.items.map(broker => ({ name: broker.bank_name, type: 'Брокерская компания' })) || [];
     const mfos = mfoData?.items.map(mfo => ({ name: mfo.bank_name, type: 'Микрофинансовая организация' })) || [];
-    const insurances = insurancesData?.items.map(insurance => ({ name: insurance.bank_name, type: 'Страховая компания' })) || [];
+    const insurances = insurancesData?.items.map(insurance => ({
+      name: insurance.bank_name,
+      type: 'Страховая компания',
+    })) || [];
     return [...banks, ...brokers, ...mfos, ...insurances];
   }, [banksData, brokersData, mfoData, insurancesData]);
 
-  const allTypes = ['Страховая компания', 'Микрофинансовая организация', 'Брокерская компания', 'Банк'];
-
-  if (isLoading) return <CircularProgress />;
+  if (isLoading) return <CircularProgress data-testid="loading" />;
 
   return (
     <div>
       <div className="text-4xl mt-4 font-semibold">Список компаний</div>
-      <CompaniesTable companies={companies} allTypes={allTypes} />
+      <CompaniesTable companies={companies} allTypes={allCompanyTypes} />
     </div>
   );
 };
